99c89d8a352880df65824f3e55c1041af3d62c9a
Mark Brown <az-dev1@outlook.com>
2023-03-31 18:32:59 +0100

    Initial Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <div class="text-h4 font-weight-light mb-n1">Welcome to</div>
+
+      <h1 class="text-h2 font-weight-bold">AzDev.Space</h1>
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-container class="fill-height">
+        <v-responsive class="d-flex align-center text-center fill-height">
+hi about
+        </v-responsive>
+    </v-container>
+</template>
+  
+<script setup>
+
+</script>
