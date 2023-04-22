5ef142da1186bfc50069e5f984e8f9888c42bfc9
Mark Brown <mark@azdev.space>
2023-04-22 21:29:42 +0100

    Added profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,251 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="12" md="3">
-                Profile (Coming Soon)
+    <v-container class="pa-8">
+        <v-row class=" d-flex justify-center">
+            <v-col>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-3">
+                            Mark Brown, BSc (Hons)
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        Tenacious, empathetic, helpful, studious and funny.
+                    </v-card-text>
+                </v-card>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-4">
+                            Who am I?
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        A dedicated and friendly developer who aligns teams, technology and processes to enable the
+                        efficient
+                        delivery of quality products.
+                    </v-card-text>
+                </v-card>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-4">
+                            What do I do?
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        <p class="mb-2">
+                            I am a Senior Developer with 23 years of experience working with web technologies.
+                            Having worked in a wide range of industries with teams of varying skill and size using numerous development processes
+                            I have become adept at solutionising, strategising and communicating. As a result I have
+                            naturally
+                            evolved into a Technical Lead whose responsibilites have included interviewing, on-boarding,
+                            mentoring,
+                            training, trouble-shooting, scheduling, planning, delegating, budgeting, prototyping,
+                            architecting,
+                            decision making
+                            and effectively communicating with everyone ranging from interns through to board members and
+                            company
+                            owners.
+                        </p>
+                        <p>
+                            When I am not performing any of the above you can expect to find me with my sleeves rolled up
+                            working
+                            on some coding or infrastructure task usually in Visual Studio, VS Code, Powershell, Azure Devops or the Azure
+                            Portal.
+                        </p>
+                    </v-card-text>
+                </v-card>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-4">
+                            What do I want?
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        <p class="mb-2">
+                            My intrinsic motivations include peer acceptance, a sense of purpose, enjoyment and an
+                            opportunity to be the best I can be.
+                        </p>
+                        <p class="mb-2">
+                            My extrinsic motivations are a fair renumeration package, status and monetary reward e.g.
+                            bonuses and or pay reviews.
+                        </p>
+                        <p>
+                            My longer term goal is to settle into a lead role for 5 years or so before considering my next
+                            move.
+                        </p>
+                    </v-card-text>
+                </v-card>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-4">
+                            What is my ideal role?
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        <p class="mb-2">
+                            My ideal role would be as a Technical Lead in an Agile team that is open to the idea of working
+                            with an Event Based
+                            Microservices Architecture in Azure.
+                        </p>
+                        <p class="mb-2">
+                            Ideally I would be using some variation of the following:
+                        </p>
+                        <ul class="pl-6">
+                            <li>.Net 6+ with XUnit and Moq for the backend code and tests.</li>
+                            <li>Vue 3, Vite, Vuetify, Typescript, GraphQL, Axios, Pinia, Vitest and Playwright for the front
+                                end code and tests</li>
+                            <li>Microsoft Azure, Pulumi, K8s, Docker for PaaS, IaaS and IaC</li>
+                            <li>Azure SQL, Cosmo DB and Azure Storage for data persistence</li>
+                            <li>Azure Event Grid or Azure Service Bus as a Messaging Service</li>
+                            <li>BDD and TDD</li>
+                        </ul>
+                    </v-card-text>
+                </v-card>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-4">
+                            What is my greatest strength?
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        Tenacity is my greatest strength. I don't give up when faced with a problem and keep going until I
+                        have implemented the correct long term solution.
+                    </v-card-text>
+                </v-card>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-4">
+                            What is my biggest weakness?
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        Perfectionism is my biggest weakness, however I can recognise when to utilise it or make acceptable
+                        compromises.
+                    </v-card-text>
+                </v-card>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-4">
+                            Who am I inspired by?
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        <p>
+                            An eclectic selection of Epictetus, Ranulph Fiennes, Ross Edgley, Greta Thunberg, Plan
+                            B, MC Eiht, Sue Laverick, Paul Tumelty and my Grandmother.
+                        </p>
+                    </v-card-text>
+                </v-card>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-4">
+                            What is my greatest professional accomplishment?
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        <p class="mb-2">
+                            STAR format (Situation, Task, Action, Result)
+                        </p>
+                        <p class="mb-2">
+                            S. A previous employers Azure Digital Estate was a sprawling mass and due to staff turnover
+                            there was nobody left who understood it.
+                        </p>
+                        <p class="mb-2">
+                            T. Somebody needed to upskill and take control of it.
+                        </p>
+                        <p class="mb-2">
+                            A. I volunteered for the task putting my own career at risk stepping away from my comfort zone,
+                            rolling up my sleeves and immersing myself within Azure for
+                            27 months.
+                        </p>
+                        <p>
+                            R.
+                        <ul class="pl-6">
+                            <li>Uptake of the Gitflow branching strategy</li>
+                            <li>Migration of code repositories from SVN to Azure Devops</li>
+                            <li>Pull Request Process introduction</li>
+                            <li>Creation of an inventory describing the Azure Digital Estate</li>
+                            <li>Identification and phased removal of unused Azure Native Services</li>
+                            <li>Downgrading of Pricing Plans where applicable</li>
+                            <li>Cost savings in the region of £80K a year</li>
+                            <li>Identification of Azure Native Services reaching end of life i.e. Classic Cloud Services</li>
+                            <li>Implementation of Azure Governance and Policies</li>
+                            <li>Improved security using Azure Keyvault, AD and App Configuration</li>
+                            <li>Phased migration to Azure Functions, Logic Apps and App Services</li>
+                            <li>Utilisation of IaC using ARM templates to spin up environments</li>
+                            <li>Containerisation using the Azure Container Registry and Azure</li>
+                            <li>Website upgrade to an App Service behind an Application Gateway</li>
+                            <li>Web Application Firewall configuration resulting in improved site availability</li>
+                            <li>Monitoring enabled using Application Insights and Log Analytics Workspaces</li>
+                            <li>Integration with Microsoft Teams and Azure Devops for CI/CD</li>
+                            <li>Adoption of Event Based Architecture using Azure Event Grid</li>
+                            <li>Azure Storage Upgrade and refinement enabling event triggers</li>
+                            <li>Azure SQL Datasync setup</li>
+                            <li>Azure Storage Replication configuration</li>
+                            <li>Azure SignalR Service implementation</li>
+                        </ul>
+                        </p>
+                    </v-card-text>
+                </v-card>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-4">
+                            What are my hobbies?
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        <p class="mb-2">
+                            <b>Mountain Biking:</b>
+                            <v-spacer></v-spacer>
+                            I have a group of friends I ride with weekly and take trips with a few times a year. This year
+                            we will be riding in
+                            Scotland and the French Alps. I have a GoPro action camera and a drone that I use to get footage
+                            for the
+                            creation of videos.
+                        </p>
+                        <p class="mb-2">
+                            <b>Physical Training:</b>
+                            <v-spacer></v-spacer>
+                            I am training for an Iron Man so most days I either swim, run or cycle.
+                        </p>
+                        <p class="mb-2">
+                            <b>Paddle Boarding:</b>
+                            <v-spacer></v-spacer>
+                            During the warmer months I try to get on the water as
+                            often as possible. The Lake District is my preferred destination and I usually combine this with
+                            camping.
+                        </p>
+                        <p class="mb-2">
+                            <b>Yoga:</b>
+                            <v-spacer></v-spacer>
+                            A recent addition to my schedule in an attempt to avoid injury with the added benefits of
+                            relaxation and meditation.
+                        </p>
+                        <p class="mb-2">
+                            <b>Reading:</b>
+                            <v-spacer></v-spacer>
+                            This year I have read The Art of Resiliance by Ross Edgely, Building Microservices by
+                            Sam Newman, The Governor by Vanessa Frake, The Unexpected Joy of Being Sober by Catherine Gray
+                            and some of Atomic Habits by James Clear.
+                        </p>
+                    </v-card-text>
+                </v-card>
+                <v-card variant="text">
+                    <v-card-item>
+                        <v-card-title class="text-left text-cyan-accent-3">
+                            Why should you hire me?
+                        </v-card-title>
+                    </v-card-item>
+                    <v-card-text class="text-teal-accent-4">
+                        You should hire me if you value somebody with a holistical approach to problem solving. 
+                        I am somebody who likes to fully understand a problem so that I can apply a higher level of 
+                        thinking and find opportunities to do better. I don't want to just pick tasks of the board,
+                        write some code and move the task right. I want to see if there is an opportunity to exceed
+                        expectation with every task. This could manifest itself as a conversation that results in a
+                        modified requirement that produces a better feature, some automation that reduces future work,
+                        better test automation or a more efficient feature in terms of cost and or availability for example.
+                
+                    </v-card-text>
+                </v-card>
             </v-col>
         </v-row>
     </v-container>
@@ -13,3 +256,6 @@
 
 
 </script>
+
+<!--To have enough Order to feel tethered, but enough Chaos to be challenged and learn new things. This
+                is where meaning is to be found!-->
